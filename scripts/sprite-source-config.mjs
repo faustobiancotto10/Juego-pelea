f8ada74ff4cbf8379486f9de329491a1cbd3c838
faustobiancotto10 <faustobiancotto10@gmail.com>
2026-09-21 20:12:39 -0300

    feat(sprite): define El Toro admitted source contract

---
+++ scripts/sprite-source-config.mjs
@@ -0,0 +1,27 @@
+export const BODY_CANVAS = Object.freeze({ width: 320, height: 320, margin: 20, pivotX: 160, pivotY: 300 });
+export const FX_CANVAS = Object.freeze({ width: 320, height: 320, margin: 20, centerX: 160, centerY: 160 });
+export const REJECTED_ALTERNATE = '13F1BB3E-C57C-4649-B9E7-07664E5E8BE8.PNG';
+
+const rows = [
+  ['IMG-00','el-toro__IMG-00__master-seed.png','b22e581da861bee4c98601d9ceb53d5c1ea79993a0bc0f4b50a72cf5e9ca5752','master',1,1,1],
+  ['IMG-01','el-toro__IMG-01__idle.png','396e43959c197c5e57a3021751dfe752f27dd70fa89bdd81e0c22422ee8f0585','body',2,4,8],
+  ['IMG-02','el-toro__IMG-02__forward-shuffle.png','20cbc7c2c55c0b38a3195957fe7e68cc6f07386428c43ea999170cbce99523fc','body',2,4,8],
+  ['IMG-03','el-toro__IMG-03__backward-shuffle.png','26d3e001a9902f66c606e720f3bf8410d7c630bbd99880369b3590dfc3b00a43','body',2,4,8],
+  ['IMG-04','el-toro__IMG-04__crouch.png','ba844bb600f903c62d918c6e3aab4365c1372af6f36ba54d31c736ff0ed398c3','body',1,4,4],
+  ['IMG-05','el-toro__IMG-05__jump.png','9bf5fd065e3241e978334791c291a06f2a001d58f789a3b4e6d7163392dd54d3','body',2,3,6],
+  ['IMG-06','el-toro__IMG-06__basic-attack.png','453e2a9a4983c94dc9579892181ae4e2cd6f214fedbb184d7628d7871b5b05ba','body',2,3,6],
+  ['IMG-07','el-toro__IMG-07__low-attack.png','53fe7886c66a4a2ac7cecc18e9bf095faa0c174a1c5ce738b4b2f7c265dcdfa4','body',2,3,6],
+  ['IMG-08','el-toro__IMG-08__block.png','194ee89d805154b1cdfe41f55c5751fb7f829e1ac14a5ea51d569c473704693c','body',1,4,4],
+  ['IMG-09','el-toro__IMG-09__hurt-knockdown.png','9f926012b8bb8e11711b572463787a7601fa2fd1d3b32ea91c5bfaff38add572','body',2,4,8],
+  ['IMG-10','el-toro__IMG-10__topete.png','bbc0eceb11a903237d12a20a5b7b1c2eef60a02c19c836c474def1dbf7b3d8f1','body',2,4,8],
+  ['IMG-11','el-toro__IMG-11__shawarmazo.png','3bc8567f72aa50cf0df8650fcb172db385e74f8706b1f7d53cf95e4b51eeca2b','body',2,4,8],
+  ['IMG-12','el-toro__IMG-12__super-eructo.png','f91465638ff69f9a568fb4e168ad755fdc52883ae9d4cade093350ec20bc1ef5','body',2,5,10],
+  ['FX-01','el-toro__FX-01__topete-impact.png','3df8e51df3387112e611e138b577db75f283286b47bab86c43edef5b38b0dab1','fx',1,4,4],
+  ['FX-02','el-toro__FX-02__shawarmazo-projectile.png','df2bfdc4dfd0a388a70693522c5b6c125c70437dbac41effd2bd0eb02cfd7e4c','fx',2,4,8],
+  ['FX-03','el-toro__FX-03__shawarmazo-impact.png','a06588de862d431074957915770d387259d77c322425a1d02f3839e52b5bc9b5','fx',1,4,4],
+  ['FX-04','el-toro__FX-04__super-eructo-effect.png','d1f69195f325d59036176eb13a48b21e595d536e56ce32930a4100ee0d4e701b','fx',2,3,6],
+];
+
+export const SHEETS = Object.freeze(rows.map(([id,file,hash,kind,gridRows,cols,expectedFrames]) => ({
+  id, file, hash, kind, rows: gridRows, cols, expectedFrames,
+})));
